b56506d9459fb2073d3eaf9927509dec2b5e5761
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2019-12-28 15:09:47 +0530

    connection made with daabase

--- registration.php
+++ registration.php
@@ -5,7 +5,7 @@
 <form method="post" enctype="multipart/form-data" >
 Name:<input type="text" name="firstname"><br><br>
 Email:<input type="email" name="email"><br><br>
-Address:<input type="textarea" name="address"  row="10" col="10"></textarea><br><br>
+Address:<textarea name="address"  row="10" col="10"></textarea><br><br>
 Phone:<input type="phone" name="phone"><br><br>
 Gender:<input type="radio" name="gender" value="male">Male
        <input type="radio"name="gender" value="female">Female<br><br>
@@ -28,6 +28,9 @@ Password:<input type="password" name="password"><br><br>
 <html>
 
 <?php
+include 'connection.php';
+
+
 if(isset($_POST['button']))
 {
 	$name=$_POST['firstname'];
@@ -43,11 +46,20 @@ if(isset($_POST['button']))
 	$Password=$_POST['password'];
 	
 	
-	echo $name."<br>".$email."<br>".$Address."<br>".
-	$Phone."<br>".$Gender."<br>".$Qualification."<br>".$photo."<br>".$district."<br>".$Username."<br>".$Password;
+echo "INSERT INTO `registration_table`(`Email`, `Address`, `Phone`, `Gender`, `Qualification`, `photo`, `district`, `Username`, `Password`, `name`) 
+VALUES ('$email','$Address','$Phone','$Gender','$Qualification','$photo','$district','$Username','$Password','$name')";
+//exit();	
+mysqli_query($connection,"INSERT INTO `registration_table`(`Email`, `Address`, `Phone`, `Gender`, `Qualification`, `photo`, `district`, `Username`, `Password`, `name`) 
+VALUES ('$email','$Address','$Phone','$Gender','$Qualification','$photo','$district','$Username','$Password','$name')");
+
+
+
 	
-}
-	
+	//echo $name."<br>".$email."<br>".$Address."<br>".
+	//$Phone."<br>".$Gender."<br>".$Qualification."<br>".$photo."<br>".$district."<br>".$Username."<br>".$Password;
+	}
+		
+
 
 ?>
 
