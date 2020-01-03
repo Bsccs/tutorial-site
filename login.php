888d38311dc795f4f94fbf271c39b9c6584a4cc0
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-01-03 16:14:39 +0530

    session done

--- login.php
+++ login.php
@@ -12,23 +12,27 @@ Password:<input type="password" name="password"><br><br>
 
 <?php
 include 'connection.php';
+session_start();
 if(isset($_POST['button']))
 {
 	$Username=$_POST['usename'];
 	$Password=$_POST['password'];
-	$result=mysqli_query($connection,"SELECT `Username`, `Password` 
+	$result=mysqli_query($connection,"SELECT * 
 	FROM `registration_table` WHERE  Username ='".$Username."'AND Password ='".$Password."'");
 	if(mysqli_num_rows($result)==1)
 	{
-        echo " You Have Successfully Logged in";
-        exit();
+		$row_data=mysqli_fetch_assoc($result);
+
+		$_SESSION['user_id']=$row_data['login_id'];
+
+
+        header('location:logtable.php');
     }
     else
 	{
-        echo " You Have Entered Incorrect Password";
+        echo" <script> alert('You Have Entered Incorrect Password!');</script>";
         exit();
     }
-
 
 }
 ?>
