f66c4a891b0713d5826c6927d198d4455837ae21
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-01-09 22:25:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
   <title>AdminLTE 3 | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <!-- Font Awesome -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
@@ -84,3 +83,38 @@
 
 </body>
 </html>
+
+
+
+
+<?php
+include 'connection.php';
+session_start();
+if(isset($_SESSION['user_id']))
+{
+	header('location:logtable.php');
+}
+
+if(isset($_POST['button']))
+{
+	$Username=$_POST['usename'];
+	$Password=$_POST['password'];
+	$result=mysqli_query($connection,"SELECT * 
+	FROM `registration_table` WHERE  Username ='".$Username."'AND Password ='".$Password."'");
+	if(mysqli_num_rows($result)==1)
+	{
+		$row_data=mysqli_fetch_assoc($result);
+
+		$_SESSION['user_id']=$row_data['login_id'];
+
+
+        header('location:logtable.php');
+    }
+    else
+	{
+        echo" <script> alert('You Have Entered Incorrect Password!');</script>";
+        exit();
+    }
+
+}
+?>
