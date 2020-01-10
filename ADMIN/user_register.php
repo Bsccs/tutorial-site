2035d0ba9b62343333840e0f1d345c1d871ac857
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-01-10 15:45:44 +0530

    updated

--- ADMIN/user_register.php
+++ ADMIN/user_register.php
@@ -58,12 +58,12 @@ if(!isset($_SESSION['user_id']))
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Dashboard</h1>
+            <h1 class="m-0 text-dark">user register</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
+              <li class="breadcrumb-item active">user register</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
