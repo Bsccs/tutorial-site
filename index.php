b242a4b497a2ef7f13e7d495169315abfbfcbdc9
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-01-13 22:09:52 +0530

    last update

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
   <head>
     <!-- meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <!-- <link rel="stylesheet" href="css/all.css" type="text/css"> -->
+   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 
     <!-- Bootstrap CSS -->
     <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
@@ -21,15 +24,15 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-     <script src="js/jquery-3.4.1.slim.min.js"></script>
+    <!--  <script src="js/jquery-3.4.1.slim.min.js"></script>
      <script src="js/popper.min.js"></script>
-     <script src="js/bootstrap.bundle.js"></script>
+     <script src="js/bootstrap.bundle.js"></script> -->
     <!--  <script src="js/bootstrap.bundle.min.js"></script> -->
      <script src="js/bootstrap.js"></script>
      <!-- <script src="/js/bootstrap.min.js"></script> -->
-
-
-   
-     </div>
+          <!-- top-navigation -->
+       
   </body>
 </html>
+
+<?php include 'top-navigation.php' ?>
