e3f3d623371f15fefdda1d643cd39e80a2fbf0da
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-01-14 20:17:42 +0530

    new updates

--- index.php
+++ index.php
@@ -3,36 +3,16 @@
   <head>
     <!-- meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" type="text/css">
-    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
-    <!-- <link rel="stylesheet" href="css/all.css" type="text/css"> -->
-   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-
-    <!-- Bootstrap CSS -->
-    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
-    <link rel="stylesheet" href="css/bootstrap.css">
-   <!--  <link rel="stylesheet" href="css/bootstrap-grid.css"> -->
-    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
-    <!-- <link rel="stylesheet" href="css/bootstrap-reboot.css"> -->
-    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
 
     <title>HACKERZDOM</title>
-
   </head>
 
   <body>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!--  <script src="js/jquery-3.4.1.slim.min.js"></script>
-     <script src="js/popper.min.js"></script>
-     <script src="js/bootstrap.bundle.js"></script> -->
-    <!--  <script src="js/bootstrap.bundle.min.js"></script> -->
-     <script src="js/bootstrap.js"></script>
-     <!-- <script src="/js/bootstrap.min.js"></script> -->
-          <!-- top-navigation -->
-       
+	<?php include 'top-navigation.php' ?>
+	<?php include 'flip-level.php' ?>
   </body>
 </html>
 
-<?php include 'top-navigation.php' ?>
+
--- top-navigation.php
+++ top-navigation.php
@@ -1,13 +1,15 @@
  <!-- top navigation -->
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <link rel="stylesheet" href="css/fontawesome.min.css">
+ <link rel="stylesheet" href="css/all.css">
   <link rel="stylesheet" href="css/top-navigation.css"> 
         <script src="js/top-navigation.js"></script>
         <div class="topnav" id="myTopnav">
         <a href="#home" class="active">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <a href="#contact"><i class="fas fa-envelope"></i> feedback</a>
+        <a href="#about"><i class="fas fa-address-card"></i> About us</a>
+        <a href="#login"><i class="fa fa-fw fa-user"></i> Login</a>
+        <a href="#register"><i class="fas fa-registered"></i></i> register</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-       <i class="fa fa-bars"></i>
+       <i class="fas fa-bars"></i>
         </a>
         </div>
