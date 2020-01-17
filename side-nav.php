ca2ec3e32408a35379f57524532450df01d60313
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-01-17 20:36:34 +0530

    new updates

---
+++ side-nav.php
@@ -0,0 +1,14 @@
+<?php include 'top-navigation.php' ?>
+ <link rel="stylesheet" href="css/side-nav.css">
+ <!-- The sidebar -->
+<div class="sidebar">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+<!-- Page content -->
+
+  
+</div> 
--- top-navigation.php
+++ top-navigation.php
@@ -1,13 +1,15 @@
  <!-- top navigation -->
  <link rel="stylesheet" href="css/fontawesome.min.css">
  <link rel="stylesheet" href="css/all.css">
-  <link rel="stylesheet" href="css/top-navigation.css"> 
+  <link rel="stylesheet" href="css/top-navigation.css">
+  <link rel="stylesheet" href="css/login.css">
+<script src="js/login.js"></script> 
         <script src="js/top-navigation.js"></script>
         <div class="topnav" id="myTopnav">
         <a href="#home" class="active">Home</a>
         <a href="#contact"><i class="fas fa-envelope"></i> feedback</a>
         <a href="#about"><i class="fas fa-address-card"></i> About us</a>
-        <a href="#login"><i class="fa fa-fw fa-user"></i> Login</a>
+        <a href="login.php" onclick="document.getElementById('id01').style.display='block'"><i class="fa fa-fw fa-user"></i> Login</a>
         <a href="#register"><i class="fas fa-registered"></i></i> register</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
        <i class="fas fa-bars"></i>
