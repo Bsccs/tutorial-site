24b58835fca61747e0ad7311d3656490099f36ad
Joby George <52007092+jobygeorge99@users.noreply.github.com>
2020-02-09 09:06:06 +0530

    Update reg_condev.php

---
+++ site files/reg_condev.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tutorial site</title>
+</head>
+<body>
+	<h1>Register as Content Developer</h1>
+	Name:<input type="text" name="name">
+	Email:<input type="text" name="name">
+	Qualification document:
+</body>
+</html>
