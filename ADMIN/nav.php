45a3a7c38821e9d7741753eb3305e0cc5231aefd
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-02-09 14:25:39 +0530

    update navbar

--- ADMIN/nav.php
+++ ADMIN/nav.php
@@ -1,63 +1,60 @@
+<!-- navbar color:navbar-dark,responsive:lg -->
 <nav class="main-header navbar navbar-expand-lg navbar-dark navbar-light">
     <div class="container">
-      <a href="admin_dashboard.php" class="navbar-brand">
-        <!-- <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-             style="opacity: .8"> -->
-        <!-- <span class="brand-text font-weight-light">Admin</span> -->
-      </a>
-      
-      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a href="admin_dashboard.php" class="nav-link">ADMIN</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">PROFILE</a>
-          </li>
-          <li class="nav-item">
-            <a href="admin_login_details.php" class="nav-link">LOGIN-DETAILS</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">INSPECTOR</a>
-            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-              <li><a href="#" class="dropdown-item">Some action </a></li>
-              <li><a href="#" class="dropdown-item">Some other action</a></li>
+    	<!-- responsive menu icon opening --> 
+        <a href="dashboard.php" class="navbar-brand"></a>      
+      	<button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" 
+      		aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+      	</button>
+      	<div class="collapse navbar-collapse order-3" id="navbarCollapse">
+      		<!-- responsive menu icon closing-->
+        	<!-- Left side content -->
+        	<ul class="navbar-nav">
+        		<!-- admin -->
+          		<li class="nav-item"><a href="dashboard.php" class="nav-link">ADMIN</a></li>
+          		<!-- profile -->
+          		<li class="nav-item"><a href="#" class="nav-link">PROFILE</a></li>   
+          		<!-- login details     -->   		 
+          		<li class="nav-item"><a href="login_details.php" class="nav-link">LOGIN-DETAILS</a></li>
+          		<!-- inspector -->
+          		<li class="nav-item dropdown"><a id="dropdownSubMenu1" href="#" data-toggle="dropdown" 
+          			aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">INSPECTOR</a>
+            		<ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+              			<li><a href="#" class="dropdown-item">Some action </a></li>
+              			<li><a href="#" class="dropdown-item">Some other action</a></li>
+            		</ul>
+          		</li>
+          		<!-- student -->
+           		<li class="nav-item dropdown">
+            	 <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" 
+            	  class="nav-link dropdown-toggle">STUDENT</a>
+            		<ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+              			<li><a href="admin_std_reg.php" class="dropdown-item">reg-details </a></li>
+              			<li><a href="#" class="dropdown-item">Some other action</a></li>
+            		</ul>
+          		</li>
+          		<!-- teacher -->
+            	<li class="nav-item dropdown">
+            		<a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">TEACHER</a>
+            		<ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+              			<li><a href="#" class="dropdown-item">Some action </a></li>
+              			<li><a href="#" class="dropdown-item">Some other action</a></li>
+            		</ul>
+          		</li>
+          		<!-- visit site -->
+          		<li class="nav-item">
+            		<a href="#" class="nav-link">VISIT SITE</a>
+          		</li>
             </ul>
-          </li>
-           <li class="nav-item dropdown">
-            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">STUDENT</a>
-            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-              <li><a href="admin_std_reg.php" class="dropdown-item">reg-details </a></li>
-              <li><a href="#" class="dropdown-item">Some other action</a></li>
-            </ul>
-          </li>
-            <li class="nav-item dropdown">
-            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">TEACHER</a>
-            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-              <li><a href="#" class="dropdown-item">Some action </a></li>
-              <li><a href="#" class="dropdown-item">Some other action</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">VISIT SITE</a>
-          </li>
-
-        </ul>
-
-      <!-- Right navbar links -->
-      <ul class="order-1 order-md-4 navbar-nav navbar-expand ml-auto">
-        <li class="nav-item">
-        <a href="logout.php" class="nav-link"><i class="fas fa-sign-out-alt">   LOGOUT</i></a> 
-        </li>
-        
-      </ul>
-
-    </div>
-  </div>
-  </nav>
+      		<!-- Right navbar links -->
+      		<ul class="order-1 order-md-4 navbar-nav navbar-expand ml-auto">
+      			<!-- logout -->
+        		<li class="nav-item">
+        			<a href="logout.php" class="nav-link"><i class="fas fa-sign-out-alt">   LOGOUT</i></a> 
+        		</li>
+      		</ul>
+    	</div>
+  	</div>
+</nav>
 
