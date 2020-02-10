16ab13b58f80ddfc0efe5e8b12ccb178cd2d0039
Joby George <jobygeorge995@gmail.com>
2020-02-11 00:24:46 +0530

    update

--- site files/reg_condev.php
+++ site files/reg_condev.php
@@ -1,10 +1,10 @@
 
 <?php
 
-echo $_POST['name'];
+/*echo $_POST['name'];
 $file=$_FILES["document"];
 print_r($file);
-
+*/
 
 if (isset($_POST['submit']))
 {
@@ -16,17 +16,21 @@ if (isset($_POST['submit']))
 
         $target_path="uploads/";
         $target_path=$target_path.basename($_FILES['document']['name']);
+     //   echo $target_path;
         if(move_uploaded_file($_FILES["document"]["tmp_name"],$target_path)) 
         {
-            $sql="INSERT INTO `content_developer` VALUES (NULL,'$_POST['name']','$_POST['email']','$target_path','$_POST['expertise']'";
+             $name=$_POST['name'];
+             $email=$_POST["email"];
+             $expertise=$_POST['expertise'];
+             $sql="INSERT INTO `content_developer` VALUES (NULL, '$name', '$email', '$target_path', '$expertise', '0')";
             $result=mysqli_query($connection,$sql);
             if($result)
             { 
-                echo "successfully registered";
+                echo " successfully registered";
             }
             
         }
-        echo "<img src='$target_path' height='280' width='320'";
+        echo "<img src='$target_path' height='280' width='320>";
  }
 
 
