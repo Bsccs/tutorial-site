c77157326ddccc8a99ed8b7d44be96a1756844b4
Joby George <jobygeorge995@gmail.com>
2020-02-11 08:35:15 +0530

    Update registration.php

--- site files/classes/registration.php
+++ site files/classes/registration.php
@@ -21,9 +21,26 @@ class registration{
         // TODO implemet here
     }
 
-    public function add_contentdev($name,$email,$targetpath,$expertise) {
+    public function add_contentdev($name,$email,$expertise,$files) {
         // TODO implement here
 
+        $target_path="uploads/";
+        $target_path=$target_path.basename($_FILES['document']['name']);
+    //  echo $target_path;
+        if(move_uploaded_file($_FILES["document"]["tmp_name"],$target_path)) 
+        {
+            /* $name=$_POST['name'];
+             $email=$_POST["email"];
+             $expertise=$_POST['expertise'];*/
+             $sql="INSERT INTO `content_developer` VALUES (NULL, '$name', '$email', '$target_path', '$expertise', '0')";
+            $result=mysqli_query($connection,$sql);
+            if($result)
+            { 
+                echo " successfully registered";
+            }
+            
+        }
+      //  echo "<img src='$target_path' height='280' width='320>";  display failure
 
           
     }
