31083b83e4b967d0163d363d4d1cae695b73d630
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-03-18 22:24:33 +0530

    update

---
+++ STUDENT/level1.php
@@ -0,0 +1,28 @@
+<?php include'../content-provider/includes/header.php'?>
+
+	<!-- main content opening -->
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+        <title>student</title>
+</head>
+<body>
+<center><h1>student</h1></center>
+<h2>Welcome <?php echo $name; ?></h2>
+<br>
+
+
+</body>
+</html>
+
+?>
+
+
+
+	<!-- main content closing -->
+
+<?php include'includes/footer.php'?>
+
