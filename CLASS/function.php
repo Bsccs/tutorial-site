06bf4969d91fbbb40214e2f0a6352f2a1b4f617e
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-04-01 12:25:20 +0530

    Update function.php

--- CLASS/function.php
+++ CLASS/function.php
@@ -33,8 +33,7 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 // Function for signin
 public function signin($uemail,$pasword)
 	{
-	$result=mysqli_query($this->dbh,"SELECT student_details.student_id,login.login_id,role,name,email,password 
-	FROM `login` LEFT JOIN student_details ON login.login_id = student_details.login_id WHERE email ='".$uemail."'AND password ='".$pasword."'");
+	$result=mysqli_query($this->dbh,"SELECT `content_developer`.`developer_id`,`student_details`.`student_id`,`student_details`.`name`,`login`.`login_id`,`role`,`login`.`email`,`login`.`password` FROM `login` LEFT JOIN `student_details` ON `login`.`login_id` = `student_details`.`login_id` LEFT JOIN `content_developer` ON `login`.`login_id` = `content_developer`.`login_id` WHERE `login`.`email` ='".$uemail."'AND `login`.`password` ='".$pasword."'");
 	return $result;
 	}
 
