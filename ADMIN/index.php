6a4481a1d71d871654bb91ff7a9af9e49fdfc4f8
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-04-06 12:13:03 +0530

    update

--- ADMIN/index.php
+++ ADMIN/index.php
@@ -3,11 +3,32 @@ session_start();
   include_once('../site files/classes/admin.php');
 include "connect.php";
   $add_expertise=new admin($connection);
+  $num_of_content=mysqli_query($connection,"SELECT COUNT(content_name) FROM content");
+  $num_of_student=mysqli_query($connection,"SELECT COUNT(email) FROM login WHERE role='student'");
+  $num_of_teachers=mysqli_query($connection,"SELECT COUNT(name1) FROM content_developer WHERE admin_aproved='1' ");
+  $num_of_questions=mysqli_query($connection,"SELECT COUNT(question_num) FROM `tests`");
+  
+ $r1=mysqli_fetch_assoc($num_of_content);
+  $r2=mysqli_fetch_assoc($num_of_student); 
+  $r3=mysqli_fetch_assoc($num_of_teachers);
+  $r4=mysqli_fetch_assoc($num_of_questions);
+  $r1=implode($r1);
+  $r2=implode($r2);
+  $r3=implode($r3);
+  $r4=implode($r4);
   ?>
 <style>
           .layout{
             margin-top: 80px;
 
+
+        }
+        .inner
+        {
+          
+          height: 140px;
+          
+          border: 2px solid black;
         }
                 
 h1 {
@@ -23,7 +44,7 @@ h1 {
 </style>
 
 <?php include 'includes/header.php'?>
-                      <h1 class="m-0 text-light" > ADMIN</h1>
+                      <h1 class="m-0 text-light" > <u>ADMIN</u></h1>
                     </div>
                 </div>
               </div>
@@ -38,13 +59,13 @@ h1 {
                 <div class="col-lg-3 col-6">
                   <div class="small-box bg-info">
                     <div class="inner">
-                      <h3>150</h3>
-                      <p>number contents</p>
+                      <h3><?php echo $r1;?></h3>
+                      <p>NUMBER OF CONTENTS</p>
                     </div>
                     <div class="icon">
-                      <i class="ion ion-bag"></i>
+                      <i class="fas fa-book-reader"></i>
                     </div>
-                    <a href="login_details.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+<!--               <a href="login_details.php" class="small-box-footer"> More info <i class="fas fa-arrow-circle-right"></i> </a>  -->
                   </div>
                 </div>
                  <!-- ./col -->
@@ -52,13 +73,13 @@ h1 {
                  <!-- small box -->
                   <div class="small-box bg-warning">
                     <div class="inner">
-                     <h3>44</h3>
-                     <p>User Registrations</p>
+                     <h3><?php echo $r2;?></h3>
+                     <p>NUMBER OF STUDENTS</p>
                     </div>
                     <div class="icon">
-                     <i class="ion ion-person-add"></i>
+                     <i class="fas fa-user-graduate"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+  <!--                   <a href="std_reg.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
                   </div>
                 </div>
                 <!-- ./col -->
@@ -66,26 +87,26 @@ h1 {
                   <!-- small box -->
                   <div class="small-box bg-danger">
                     <div class="inner">
-                      <h3>65</h3>
-                      <p>number of teachers</p>
+                      <h3><?php echo $r3;?></h3>
+                      <p>NUMBER OF TEACHERS</p>
                     </div>
                     <div class="icon">
-                     <i class="ion ion-pie-graph"></i>
+                     <i class="fas fa-chalkboard-teacher"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+       <!--              <a href="develop_details.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
                   </div>
                 </div>
-                          <div class="col-lg-3 col-6">
+                      <div class="col-lg-3 col-6">
                   <!-- small box -->
                   <div class="small-box bg-success">
                     <div class="inner">
-                      <h3>65</h3>
-                      <p>Unique Visitors</p>
+                      <h3><?php echo $r4;?></h3>
+                      <p>NUMBER OF TEST QUESTIONS</p>
                     </div>
                     <div class="icon">
-                     <i class="ion ion-pie-graph"></i>
+                     <i class="far fa-file-alt"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+<!--                     <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
                   </div>
                 </div>
                 <!-- ./col -->
