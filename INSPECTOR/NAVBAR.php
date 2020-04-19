c5a6a011cc5ae10c1d18011aea66dee7d64fc004
hemant p hareesh <55651214+Hemantphareesh@users.noreply.github.com>
2020-04-19 09:10:55 +0530

    inpsector module updates

--- INSPECTOR/NAVBAR.php
+++ INSPECTOR/NAVBAR.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
 <div class="container">
-  <a class="navbar-brand" href="#">HACKERZDOM</a>
+  <a class="navbar-brand" href="INDEX.php">HACKERZDOM</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -8,16 +8,17 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item ">
-        <a class="nav-link" href="#">ADD-DETAILS<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="INDEX.php"><i class="fas fa-home"></i> HOME</a>
       </li>
+
       <li class="nav-item">
-        <a class="nav-link" href="approval.php">APROVAL</a>
+        <a class="nav-link" href="approval.php"><i class="fas fa-thumbs-up"></i> APPROVALS</a>
       </li>
     </ul>
     <ul class="nav navbar-nav float-md-right">
     	      <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          USER
+          <i class="fas fa-users-cog"></i> <?php echo $_SESSION['name2'];?>
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="profile.php">PROFILE</a>
--- INSPECTOR/includes/footer.php
+++ INSPECTOR/includes/footer.php
@@ -3,9 +3,10 @@
 	<!-- body-wrap-closing -->
 	</div>
 		<!--script opening-->
-		<script src="PLUGINS/jquery-3.4.1.slim.min.js"></script>
-		<script src="PLUGINS/popper.min.js"></script>
-		<script src="PLUGINS/bootstrap.min.js"></script>
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="PLUGINS/jquery-3.4.1.slim.min.js"></script>
+	<script src="PLUGINS/popper.min.js"></script>
+	<script src="PLUGINS/bootstrap.min.js"></script>
 		<!-- script closing -->
 	</body>
 	<!-- body part closing ---->
